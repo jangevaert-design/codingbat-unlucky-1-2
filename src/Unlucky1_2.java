dde41b2e36bf160bec3f9adb1bba7fe55682ab69
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-28 05:05:25 -0600

    Initial commit

---
+++ src/Unlucky1_2.java
@@ -0,0 +1,11 @@
+public class Unlucky1_2 {
+  public boolean unlucky1(int[] nums) {
+    int len = nums.length;
+    if (len <= 1) {
+      return false;
+    }
+
+    return (nums[0] == 1 && nums[1] == 3) || (nums[1] == 1 && nums[2] == 3)
+        || (nums[len - 2] == 1 && nums[len - 1] == 3);
+  }
+}
